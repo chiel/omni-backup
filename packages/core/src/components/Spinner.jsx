a13529577158b1a05c6acf6746d87097d865f996
Chiel Kunkels <chiel@kunkels.me>
2017-09-02 23:43:43 +0200

    core: allow Spinner to receive a message

--- packages/core/src/components/Spinner.jsx
+++ packages/core/src/components/Spinner.jsx
@@ -1,14 +1,31 @@
+import PT from 'prop-types';
 import React from 'react';
 
 import css from '../styles/spinner.css';
 
-export default function Spinner() {
-	return (
-		<div className={css.container}>
-			<div className={css.cube1} />
-			<div className={css.cube2} />
-			<div className={css.cube4} />
-			<div className={css.cube3} />
-		</div>
-	);
+export default class Spinner extends React.PureComponent {
+	static propTypes = {
+		message: PT.string,
+	};
+
+	static defaultProps = {
+		message: '',
+	};
+
+	render() {
+		const { message } = this.props;
+		return (
+			<div className={css.container}>
+				<div className={css.spinner}>
+					<div className={css.cube1} />
+					<div className={css.cube2} />
+					<div className={css.cube4} />
+					<div className={css.cube3} />
+				</div>
+				{message !== '' && (
+					<p className={css.message}>{message}</p>
+				)}
+			</div>
+		);
+	}
 }
