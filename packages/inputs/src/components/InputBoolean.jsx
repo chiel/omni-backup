ffcb89fab85d441ad72f26192f9eea10e4a5147d
Lukankie <eentweedrie@gmail.com>
2017-09-05 16:09:06 +0200

    inputs: add boolean component and register to the list of inputs

---
+++ packages/inputs/src/components/InputBoolean.jsx
@@ -0,0 +1,49 @@
+import PT from 'prop-types';
+import React from 'react';
+
+import withField from './Field';
+
+import css from '../styles/input.css';
+
+export class InputBoolean extends React.PureComponent {
+	static propTypes = {
+		name: PT.string.isRequired,
+		onChange: PT.func,
+		onUpdate: PT.func.isRequired,
+	};
+
+	static defaultProps = {
+		onChange: () => {},
+	};
+
+	handleChange = ev => {
+		this.props.onUpdate(ev.target.value);
+		this.props.onChange(ev);
+	}
+
+	render() {
+		const { ...props } = this.props;
+		const sliderId = `slider-${this.props.name}`;
+
+		delete props.onUpdate;
+
+		return (
+			<div>
+				<label
+					className={css.toggle}
+					htmlFor={sliderId}
+				>
+					<input
+						{...props}
+						id={sliderId}
+						onChange={this.handleChange}
+						type="checkbox"
+					/>
+					<div className={css.slider} />
+				</label>
+			</div>
+		);
+	}
+}
+
+export default withField(InputBoolean);
