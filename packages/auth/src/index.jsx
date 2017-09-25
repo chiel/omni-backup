3933888fa3179e74a42f6c435d553ac6e3222aa1
Chiel Kunkels <chiel@kunkels.me>
2017-09-25 22:38:48 +0200

    auth: add method to create jwt

--- packages/auth/src/index.jsx
+++ packages/auth/src/index.jsx
@@ -1,7 +1,10 @@
+import jwt from 'jsonwebtoken';
 import React from 'react';
 
 import Login from './components/Login';
 import providers from './providers';
+
+const salt = process.env.AUTH_TOKEN_SALT;
 
 export default function authPlugin(omni) {
 	omni.addRoute({
@@ -13,5 +16,14 @@ export default function authPlugin(omni) {
 		providers.push({ Component, name });
 	};
 
-	omni.auth = { addProvider };
+	const createToken = (subject, secret, payload = {}) => (
+		new Promise((resolve, reject) => {
+			jwt.sign(payload, salt + secret, { subject: `${subject}` }, (err, token) => {
+				if (err) return reject(err);
+				resolve(token);
+			});
+		})
+	);
+
+	omni.auth = { addProvider, createToken };
 }
