5eae87ffb75f318b5f068453303df1b660556fcc
Chiel Kunkels <chiel@kunkels.me>
2017-11-09 23:29:51 +0100

    user: use the createTable helper function

--- packages/user/src/server.js
+++ packages/user/src/server.js
@@ -1,23 +1,16 @@
-/* eslint-disable indent */
 const createTableQuery = (
-`create table if not exists user (
-	id int(11) not null auto_increment,
-	first_name varchar(50) not null,
-	last_name varchar(50) not null,
-	email varchar(255) not null,
-	image text null,
-	superadmin boolean not null default false,
-	created_at datetime not null default now(),
-	primary key (id)
-) engine=InnoDB default charset=utf8 collate=utf8_unicode_ci;`
+	`create table if not exists user (
+		id int(11) not null auto_increment,
+		first_name varchar(50) not null,
+		last_name varchar(50) not null,
+		email varchar(255) not null,
+		image text null,
+		superadmin boolean not null default false,
+		created_at datetime not null default now(),
+		primary key (id)
+	) engine=InnoDB default charset=utf8 collate=utf8_unicode_ci;`
 );
-/* eslint-enable indent */
 
 export default function userPlugin(omni) {
-	omni.mysql.query(createTableQuery)
-		.then(result => {
-			if (result.warningCount === 0) {
-				console.info('Created user table');
-			}
-		});
+	omni.mysql.createTable('user', createTableQuery);
 }
